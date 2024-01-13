d0f108bad82091563f6bc1d8c69157059a50515f
Wagner Xu <wagnerhsu@hotmail.com>
2024-01-13 21:51:27 +0800

    feat: add 08-ExceptionHandling

---
+++ MyCode/08-ExceptionHandling/EitherTypeDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using LanguageExt;
+using static System.Console;
+
+Test2(5);
+Test2("Hello");
+
+static void Test2(Either<string, int> val)
+{
+    val.Match
+    (
+        Right: x => WriteLine($"Correct value: {x}"),
+        Left: x => WriteLine($"Incorrect value: '{x}'")
+    );
+}
